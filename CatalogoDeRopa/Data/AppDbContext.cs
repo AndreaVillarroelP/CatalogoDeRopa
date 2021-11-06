04d2672b36be3700e104ee4e599401d65823de25
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-11-05 22:04:05 -0400

    Carpeta Data Agregada

---
+++ CatalogoDeRopa/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CatalogoDeRopa.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace CatalogoDeRopa.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Catalogo> Catalogo { get; set; }
+
+    }
+}
